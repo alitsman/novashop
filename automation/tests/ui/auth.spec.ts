9718e1bdeecc65f2f5f8a1d915175153b5ede754
alitsman <alekseilitsman@gmail.com>
2026-07-20 14:42:53 +0400

    test: add login scenarios

--- automation/tests/ui/auth.spec.ts
+++ automation/tests/ui/auth.spec.ts
@@ -1,13 +1,16 @@
 import { test, expect } from "@playwright/test";
 
-test.describe("authentication", () => {
-  test("shows an error for invalid credentials", async ({ page }) => {
+test.describe("login", () => {
+  test.beforeEach(async ({ page }) => {
     await page.goto("/login");
+  });
 
+  test("invalid credentials: shows an error", async ({ page }) => {
     const emailInput = page.getByLabel("Email address");
     const passwordInput = page.getByLabel("Password");
     const signInButton = page.getByRole("button", {
       name: "Sign in",
+      exact: true,
     });
     const loginError = page.getByRole("alert");
 
@@ -16,7 +19,29 @@ test.describe("authentication", () => {
     await signInButton.click();
 
     await expect(page).toHaveURL("/login");
-    await expect(loginError).toBeVisible();
     await expect(loginError).toHaveText("Email address or password is incorrect.");
+  });
+
+  test("regular user: signs in successfully", async ({ page }) => {
+    const emailInput = page.getByLabel("Email address");
+    const passwordInput = page.getByLabel("Password");
+    const signInButton = page.getByRole("button", {
+      name: "Sign in",
+      exact: true,
+    });
+
+    await emailInput.fill("user@test.com");
+    await passwordInput.fill("user123");
+    await signInButton.click();
+
+    const userName = page.locator(".app-header__user-name");
+    const logoutButton = page.getByRole("button", {
+      name: "Logout",
+      exact: true,
+    });
+
+    await expect(page).toHaveURL("/products");
+    await expect(userName).toHaveText("Regular User");
+    await expect(logoutButton).toBeVisible();
   });
 });
